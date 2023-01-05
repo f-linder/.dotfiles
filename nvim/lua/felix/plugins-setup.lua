0a03057391ce974f5deef3a46e7de111e8571857
Felix Linder <felix@pop-os.localdomain>
2023-01-05 18:36:14 +0100

    lua setup

---
+++ nvim/lua/felix/plugins-setup.lua
@@ -0,0 +1,100 @@
+-- auto install packer if not installed
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+        vim.cmd([[packadd packer.nvim]])
+        return true
+    end
+    return false
+end
+local packer_bootstrap = ensure_packer() -- true if packer was just installed
+
+-- autocommand that reloads neovim and installs/updates/removes plugins
+-- when file is saved
+vim.cmd([[ 
+    augroup packer_user_config
+        autocmd!
+        autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
+    augroup end
+]])
+
+local status,packer = pcall(require, "packer")
+if not status then
+    return
+end
+
+-- list of plugins
+return packer.startup(function(use)
+    use("wbthomason/packer.nvim") -- packer / plugin manager itself
+
+    use("nvim-lua/plenary.nvim") -- lua functions that many plugins use
+
+    use("bluz71/vim-nightfly-guicolors") -- colorscheme
+
+    use("christoomey/vim-tmux-navigator") -- tmux & split window navigation
+
+    use("szw/vim-maximizer") -- maximizes and restores current window
+
+    use("tpope/vim-surround") -- add, delete, change surroundings 
+
+    use("numToStr/Comment.nvim") -- commenting with gc
+
+    use("nvim-tree/nvim-tree.lua") -- file explorer
+
+    use("nvim-tree/nvim-web-devicons") -- vs-code like icons
+
+    use("nvim-lualine/lualine.nvim") -- statusline
+
+    -- fuzzy finder w/ telescope
+    use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+    use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
+
+    -- autocompletion
+    use("hrsh7th/nvim-cmp") -- completion plugin
+    use("hrsh7th/cmp-buffer") -- source for text in buffer
+    use("hrsh7th/cmp-path") -- source for file system paths
+
+    -- snippets
+    use("L3MON4D3/LuaSnip") -- snippet engine
+    use("saadparwaiz1/cmp_luasnip") -- for autocompletion
+    use("rafamadriz/friendly-snippets") -- useful snippets
+
+    -- managing & installing lsp
+    use("williamboman/mason.nvim") -- in charge of managing lsp servers, linters & formatters
+    use("williamboman/mason-lspconfig.nvim") -- bridges gap b/w mason & lspconfig
+
+    -- configuirng lsp servers
+    use("neovim/nvim-lspconfig") -- easily configure language servers
+    use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
+    use({ "glepnir/lspsaga.nvim", branch = "main" }) -- enhanced lsp uis
+    use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
+
+    -- formatiing + linting
+    use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
+    use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
+
+    -- treesitter
+    use {"nvim-treesitter/nvim-treesitter",
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+    }
+
+    -- auto closing
+    use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
+    use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
+
+    -- git integration
+    use("lewis6991/gitsigns.nvim") -- show line modification on left hand side
+
+    -- tabbing out of matching symbols
+    use("abecodes/tabout.nvim")
+
+
+    if packer_bootstrap then
+        require("packer").sync()
+    end
+end)
---
+++ nvim/lua/felix/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+-- import nvim-treesitter plugin safely
+local status, treesitter = pcall(require, "nvim-treesitter.configs")
+if not status then
+  return
+end
+
+-- configure treesitter
+treesitter.setup({
+    -- enable syntax highlighting
+    highlight = {
+        enable = true,
+    },
+    -- enable indentation
+    indent = { enable = true },
+})
